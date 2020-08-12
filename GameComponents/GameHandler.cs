417a4bcc8553cbd0231d9c35be1476354bbfa031
Kevin <kevin005005@gmail.com>
2020-08-13 00:55:04 +0200

    Many rules added, code optimized.

--- GameComponents/GameHandler.cs
+++ GameComponents/GameHandler.cs
@@ -10,7 +10,7 @@ namespace Karata.GameComponents
 {
     class GameHandler
     {
-        int currentPlayerId;
+        int currentPlayerId = 0;
 
         Player currentPlayer
         {
@@ -25,12 +25,14 @@ namespace Karata.GameComponents
         GameDeck deck;
 
         // The card that the last player disposed of.
-        Card lastCard;
+        Card lastCard = new Card();
 
         // Mode for the next turn.
-        Draw mode;
+        Draw mode = Draw.None;
 
-        bool clockWise;
+        bool samePlayerComes = false;
+
+        bool clockWise = true;
 
         bool gameOver
         {
@@ -49,12 +51,6 @@ namespace Karata.GameComponents
 
         public GameHandler(int numberOfPlayers, int numberOfDecks)
         {
-            clockWise = true;
-
-            lastCard = new Card();
-
-            mode = Draw.None;
-
             deck = new GameDeck(numberOfDecks);
 
             players = new Player[numberOfPlayers];
@@ -68,9 +64,7 @@ namespace Karata.GameComponents
                     players[i].PlayerCards.Push(deck.Pop());
                 }
             }
-
-            currentPlayerId = 0;
-
+            
             StartGame();
         }
 
@@ -91,48 +85,121 @@ namespace Karata.GameComponents
                 Console.WriteLine("Draw a card (d) or put a card down (number of the card)");
                 string input = Console.ReadLine();
 
-                if (input == "d")
+                // If the player cannot put down any card.
+                bool playerMustDraw = true;
+
+                foreach (Card card in currentPlayer.PlayerCards)
                 {
-                    currentPlayer.PlayerCards.Push(deck.Pop());
+                    if (ValidatePlayerCard(card))
+                    {
+                        playerMustDraw = false;
+                        break;
+                    }
+                }
+
+                if (input == "d" || playerMustDraw)
+                {
+                    int drawThisManyTimes = 0;
+                    switch (mode)
+                    {
+                        case Draw.None:
+                            drawThisManyTimes = 1;
+                            break;
+                        case Draw.Two:
+                            drawThisManyTimes = 2;
+                            break;
+                        case Draw.Three:
+                            drawThisManyTimes = 3;
+                            break;
+                        case Draw.Five:
+                            drawThisManyTimes = 5;
+                            break;
+                    }
+
+                    for (int i = 0; i < drawThisManyTimes; i++)
+                    {
+                        currentPlayer.PlayerCards.Push(deck.Pop());
+                    }
                 }
                 else if (int.TryParse(input, out int result))
                 {
-                    if (result < 0 || result >= currentPlayer.PlayerCards.Size)
-                    {
-                        continue;
-                    }
-                    else
+                    if (result >= 0 || result < currentPlayer.PlayerCards.Size)
                     {
                         if (ValidatePlayerCard(currentPlayer.PlayerCards.GetAt(result)))
                         {
                             Card currentCard = currentPlayer.PlayerCards.PopAt(result);
-                            if (currentCard.Name == CardName.Ace && mode == Draw.None)
+                            switch (currentCard.Name)
                             {
-                                bool choiceValid = false;
-                                int chooseType;
-                                do
-                                {
-                                    Console.WriteLine("Hearts, spades, diamonds or clubs?");
-                                    choiceValid = int.TryParse(Console.ReadLine(), out chooseType);
-                                    if (choiceValid)
+                                case CardName.Ace:
+                                    if (mode == Draw.None)
                                     {
-                                        if (chooseType >= 0 && chooseType < 4)
+                                        bool choiceValid = false;
+                                        int chooseType;
+                                        do
                                         {
-                                            choiceValid = true;
-                                        }
+                                            Console.WriteLine("Hearts, spades, diamonds or clubs?");
+                                            choiceValid = int.TryParse(Console.ReadLine(), out chooseType);
+                                            if (choiceValid)
+                                            {
+                                                if (chooseType >= 0 && chooseType < 4)
+                                                {
+                                                    choiceValid = true;
+                                                }
+                                            }
+                                        } while (!choiceValid);
+                                        currentCard = new Card(CardName.Ace, (CardType)chooseType);
                                     }
-                                } while (!choiceValid);
-                                lastCard = new Card(CardName.Ace, (CardType)chooseType);
+                                    else
+                                    {
+                                        mode = Draw.None;
+                                        currentCard = new Card();
+                                    }
+                                    break;
+                                case CardName.Two:
+                                    mode = Draw.Two;
+                                    break;
+                                case CardName.Three:
+                                    mode = Draw.Three;
+                                    break;
+                                case CardName.Jack:
+                                    NextPlayer();
+                                    break;
+                                case CardName.Eight:
+                                case CardName.Queen:
+                                    samePlayerComes = true;
+                                    break;
+                                case CardName.King:
+                                    clockWise = !clockWise;
+                                    break;
+                                case CardName.Joker:
+                                    mode = Draw.Five;
+                                    break;
                             }
+
+                            // In all cases, the current card will be changed accordingly then be assigned to last card.
+                            lastCard = currentCard;
                         }
+                    }
+                    else
+                    {
+                        // If the user input is incorrect, prompt the user again.
+                        continue;
                     }
                 }
                 else
                 {
+                    // If the user input is incorrect, prompt the user again.
                     continue;
                 }
 
-                NextPlayer();
+                if (!samePlayerComes)
+                {
+                    NextPlayer();
+                }
+                else
+                {
+                    samePlayerComes = false;
+                }
             }
 
             GameOver();
@@ -172,6 +239,7 @@ namespace Karata.GameComponents
 
         private bool ValidatePlayerCard(Card c)
         {
+            // TODO: add validation rules
             if (mode == Draw.None)
             {
 
@@ -180,7 +248,7 @@ namespace Karata.GameComponents
             {
 
             }
-            return false;
+            return true;
         }
     }
 }
